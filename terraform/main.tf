b8879bdaa8bdb9b3f94b5a5fb8b8d8e50bdb6cea
Brynn Park <pbh7080@gmail.com>
2024-12-17 00:07:11 +0900

    Add terraform and workflows

---
+++ terraform/main.tf
@@ -0,0 +1,34 @@
+resource "null_resource" "delete_all_objects" {
+  # Use the local-exec provisioner to run the AWS CLI command
+  provisioner "local-exec" {
+    command = "echo 'Deleting all objects in S3 bucket' && aws s3 rm s3://${var.static_bucket_name} --recursive && sleep 3"
+  }
+
+  # Add a trigger to ensure this command runs when you apply changes
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+}
+
+resource "aws_s3_object" "upload-build-file" {
+    depends_on = [null_resource.delete_all_objects]
+    for_each        = fileset("../../build/", "**")
+
+    bucket          = "${var.static_bucket_name}"
+    key             = each.value
+    source          = "../../build/${each.value}"
+
+    content_type = lookup({
+    "html" = "text/html",
+    "css"  = "text/css",
+    "js"   = "application/javascript",
+    "png"  = "image/png",
+    "jpg"  = "image/jpg",
+    "jpeg" = "image/jpeg",
+    "gif"  = "image/gif",
+    "svg"  = "image/svg+xml",
+    "ico"  = "image/x-icon",
+    "json" = "application/json",
+    "txt" = "text/txt"
+  }, split(".", each.value)[length(split(".", each.value)) - 1], "application/octet-stream")
+}

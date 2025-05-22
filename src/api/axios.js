13680a602e7820d9d6b19188acfe01f25c646588
sonjoohee <90837976+sonjoohee@users.noreply.github.com>
2025-05-22 20:14:56 +0900

    delete

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL:'https://iteamoa-api.brynnpark.cloud',
+  baseURL:'https://api-iteamoa.brynnpark.cloud',
   headers: { 'Content-Type': 'application/json' }
 });
-
 
 export default instance;
